a89854aa186b4670d62637901d5e699de075fcca
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-04-03 04:16:27 +0530

    feat: header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import "./App.css";
+import { Header } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import {
+  RainbowKitProvider,
+  darkTheme,
+  getDefaultWallets,
+} from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  Number(process.env.REACT_APP_IS_MAINNET) ? [] : [polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_API }),
+    publicProvider(),
+  ]
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const { connectors } = getDefaultWallets({
+  appName: "Submit Nebulas",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={darkTheme({ accentColor: "#2fa3ff" })}
+      >
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
+);
