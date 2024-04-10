57cf766044f4c5c0154dcf594411bc40f0831af1
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-04-11 03:03:01 +0530

    style: design refactor

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,35 @@
 import "./Header.css";
 import Logo from "./logo.webp";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useConnectModal } from "@rainbow-me/rainbowkit";
 
 export function Header() {
+  const { openConnectModal } = useConnectModal();
+
   return (
     <header className="header-container">
       <a href="/">
         <img src={Logo} alt="Morty Logo" className="logo" />
       </a>
 
-      <div className="header-button__container">
-        <ConnectButton chainStatus="icon" showBalance={false} />
-      </div>
+      <ul className="header-nav__container">
+        <li>
+          <a href="/">Galielo Protocol</a>
+        </li>
+
+        <li>
+          <a href="/">Nebula Odyssey</a>
+        </li>
+
+        <li>
+          <a href="/">Opensea</a>
+        </li>
+
+        <li>
+          <button className="header-button__connect" onClick={openConnectModal}>
+            <span>Connect</span>
+          </button>
+        </li>
+      </ul>
     </header>
   );
 }
