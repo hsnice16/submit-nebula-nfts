942ff2b0f9333f0f87779224fbd49341c9fa972a
Himanshu Singh <hsnice16@gmail.com>
2024-04-11 19:36:39 +0530

    feat: faq (#10)
    Co-authored-by: Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import "./Header.css";
 import Logo from "./logo.webp";
-import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import { useConnectModal, useAccountModal } from "@rainbow-me/rainbowkit";
 
-export function Header() {
+function sliceAddress(address, visibleChars = 3) {
+  return (
+    address.slice(0, visibleChars) +
+    "..." +
+    address.slice(address.length - visibleChars, address.length)
+  );
+}
+
+export function Header({ isMobile }) {
   const { openConnectModal } = useConnectModal();
+  const { address } = useAccount();
+  const { openAccountModal } = useAccountModal();
 
   return (
     <header className="header-container">
@@ -11,25 +22,30 @@ export function Header() {
         <img src={Logo} alt="Morty Logo" className="logo" />
       </a>
 
-      <ul className="header-nav__container">
-        <li>
-          <a href="/">Galielo Protocol</a>
-        </li>
+      {isMobile ? null : (
+        <ul className="header-nav__container">
+          <li>
+            <a href="/">Galielo Protocol</a>
+          </li>
 
-        <li>
-          <a href="/">Nebula Odyssey</a>
-        </li>
+          <li>
+            <a href="/">Nebula Odyssey</a>
+          </li>
 
-        <li>
-          <a href="/">Opensea</a>
-        </li>
+          <li>
+            <a href="/">Opensea</a>
+          </li>
 
-        <li>
-          <button className="header-button__connect" onClick={openConnectModal}>
-            <span>Connect</span>
-          </button>
-        </li>
-      </ul>
+          <li>
+            <button
+              className="header-button__connect"
+              onClick={address ? openAccountModal : openConnectModal}
+            >
+              <span> {address ? sliceAddress(address, 4) : "Connect"}</span>
+            </button>
+          </li>
+        </ul>
+      )}
     </header>
   );
 }
--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,5 +1,5 @@
 import "./Landing.css";
-import { Carousel, Steps, Banner } from "../index";
+import { Carousel, Steps, FAQ } from "../index";
 import { useGetSubmittedNebulasCount } from "../../hooks";
 import YouTube from "../success/youtube.svg";
 
@@ -8,27 +8,24 @@ export function Landing() {
 
   return (
     <div className="main-sub__container landing-container">
-      <Banner submittedNebulasCount={submittedNebulasCount} />
       <h1 className="main-sub__container-head">
         Welcome to Nebula Odyssey: Upgrade Your Collection!
       </h1>
 
       <Carousel />
-      <p className="main-sub__container-para">
-        Follow the steps below to seamlessly submit your older nebulas{" "}
-      </p>
+      {submittedNebulasCount ? (
+        <h1 className="main-sub__container-head">
+          {submittedNebulasCount} Nebulas submitted so far
+        </h1>
+      ) : null}
 
       <h2 className="main-sub__container-head">How does it work? </h2>
       <a className="landing-link__youtube" href="/">
         view on <img src={YouTube} alt="youtube logo" /> YouTube
       </a>
-      <Steps />
 
-      <h4 className="landing-h4">Got Questions?</h4>
-      <p className="main-sub__container-para">
-        If you have any questions or need assistance along the way, reach out to
-        us at hello@galileoprotocol.io or from our telegram group
-      </p>
+      <Steps />
+      <FAQ />
     </div>
   );
 }
